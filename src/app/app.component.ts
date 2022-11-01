e017ab60999bfd9421289a4aa77daba0834fbe76
Iulia Nistor <iulia.nistor12@yahoo.com>
2022-11-01 15:50:41 +0000

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pka-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'poke-app';
+}
