1edb4aa96d6cdaa9282318664a612f5178fe1886
Iulia Nistor <iulia.nistor12@yahoo.com>
2022-11-03 18:56:55 +0000

    chore: comment unused test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,12 +22,12 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('poke-app');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'poke-app app is running!'
-    );
-  });
+  // it('should render title', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement as HTMLElement;
+  //   expect(compiled.querySelector('.content span')?.textContent).toContain(
+  //     'poke-app app is running!'
+  //   );
+  // });
 });
