7cac3dd5e9479b566d8cde08069c990c648bc0f0
Iulia Nistor <iulia.nistor12@yahoo.com>
2022-11-28 18:15:13 +0000

    chore: add test

--- src/app/pokemon-profile/pokemon-profile.component.spec.ts
+++ src/app/pokemon-profile/pokemon-profile.component.spec.ts
@@ -1,11 +1,15 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { pokemonMockTransformed } from '../pokemonMock';
 import { PokemonDataService } from '../services/pokemon-data.service';
 
 import { PokemonProfileComponent } from './pokemon-profile.component';
 
 describe('PokemonProfileComponent', () => {
+  let spy: jasmine.Spy;
   let component: PokemonProfileComponent;
   let fixture: ComponentFixture<PokemonProfileComponent>;
   let service: PokemonDataService;
@@ -13,16 +17,8 @@ describe('PokemonProfileComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [PokemonProfileComponent],
-      providers: [
-        PokemonDataService,
-        {
-          provide: ActivatedRoute,
-          useValue: {
-            snapshot: { params: { name: 'bulbasaur' } },
-          },
-        },
-      ],
-      imports: [HttpClientTestingModule],
+      providers: [PokemonDataService],
+      imports: [HttpClientTestingModule, RouterTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PokemonProfileComponent);
@@ -32,5 +28,14 @@ describe('PokemonProfileComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set pokemon from the service directly', () => {
+    spy = spyOn(service, 'getPokemonData').and.returnValue(
+      of(pokemonMockTransformed)
+    );
+    fixture.detectChanges();
+    expect(component.pokemonProfileData.weight).toBe(69);
+    expect(spy).toHaveBeenCalledTimes(1);
   });
 });
